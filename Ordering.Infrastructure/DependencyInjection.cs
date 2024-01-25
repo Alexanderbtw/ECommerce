dc3c276bdd3c7bae7e25a792a0db0e205120d032
Alexanderbtw <sgoldebaev@gmail.com>
2024-01-26 02:40:43 +0300

    Changed db connecting method

--- Ordering.Infrastructure/DependencyInjection.cs
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -18,7 +18,14 @@ namespace Ordering.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services,
             IConfiguration configuration)
         {
-            services.AddDbContext<OrderingContext>(options => options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString"),
+            string host = Environment.GetEnvironmentVariable("DB_HOST")!;
+            string port = Environment.GetEnvironmentVariable("DB_PORT")!;
+            string db_name = Environment.GetEnvironmentVariable("DB_DATABASE")!;
+            string db_username = Environment.GetEnvironmentVariable("DB_USERNAME")!;
+            string db_password = Environment.GetEnvironmentVariable("DB_PASSWORD")!;
+            string db_setting = String.Format("Host={0}; Port={1}; User ID={2}; Password={3}; Database={4}; CommandTimeout=300;", host, port, db_username, db_password, db_name);
+
+            services.AddDbContext<OrderingContext>(options => options.UseNpgsql(db_setting,
                 b => b.MigrationsAssembly(typeof(OrderingContext).Assembly.FullName)
                 ));
 
